7438efc518ef388f205d09c19d099ef4b055a343
Vikram Ruppa-Kasani <vr82@duke.edu>
2021-10-13 02:16:07 -0400

    Matched product.py code to match DB Schema. Rippled change to index.py's call of Product table.

--- app/models/product.py
+++ app/models/product.py
@@ -2,28 +2,42 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, id, name, price, available):
-        self.id = id
+    def __init__(self, name, category_name, image_url, description):
         self.name = name
-        self.price = price
-        self.available = available
+        self.category_name = category_name
+        self.image_url = image_url
+        self.description = description
 
     @staticmethod
-    def get(id):
+    def get(name):
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE id = :id
-''',
-                              id=id)
+        SELECT name, category_name, image_url, description
+        FROM Product
+        WHERE name = :name
+        ''',
+        name=name)
         return Product(*(rows[0])) if rows is not None else None
 
+    '''TODO(Karan (Products Guru)):
+    Since we diverged from the given db schema this static
+    method is broken. You will probably need to change the query to
+    make it compataible with our schema. I've added a placeholder static
+    method so that the rest of the app will run in the intermin - Vikram.
+
+    '''
     @staticmethod
-    def get_all(available=True):
+    def get_all():
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE available = :available
-''',
-                              available=available)
-        return [Product(*row) for row in rows]
+        SELECT name, category_name, image_url, description
+        FROM Product
+        ''')
+        return Product(*(rows[0])) if rows is not None else None
+#     @staticmethod
+#     def get_all(available=True):
+#         rows = app.db.execute('''
+# SELECT id, name, price, available
+# FROM Products
+# WHERE available = :available
+# ''',
+#                               available=available)
+#         return [Product(*row) for row in rows]
