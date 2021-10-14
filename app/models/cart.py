8425477b9d43ca2d982a42bcdfb33bcd6efb803d
sj253 <sj253@duke.edu>
2021-10-14 18:40:37 -0400

    Update cart.py

--- app/models/cart.py
+++ app/models/cart.py
@@ -1,1 +1,13 @@
+from flask import current_app as app
+class Cart:
+    def __init__(self, item_id):
+        self.item_id = item_id
 
+    @staticmethod
+    def getOrders(id):
+        rows = app.db.execute('''
+        SELECT orders
+        FROM Cart
+        WHERE name = :profile
+        ''',
+        name=name)
