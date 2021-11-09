459ee9df6a1c28896a1ce587ca0c0ee4629505a9
bes41 <bes41@duke.edu>
2021-11-09 09:47:24 -0500

    User dash

--- app/index.py
+++ app/index.py
@@ -19,19 +19,9 @@ def index():
     # get all available products for sale:
     products = Product.get_all(True)
 
-    # find the products current user has bought:
-    if current_user.is_authenticated:
-        purchases = []
-    #     purchases = Purchase.get_all_by_uid_since(
-    #         current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    else:
-        purchases = None
-
-
     # render the page by adding information to the index.html file
     return render_template('index.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+                           avail_products=products)
 
 
 @bp.route('/seller-inventory/<user_id>', methods=['GET'])
