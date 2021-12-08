7ad36e3fadac4012df252bbeea43fb9d7cca6dfc
Vikram Ruppa-Kasani <vr82@duke.edu>
2021-12-08 12:49:41 -0500

    new seller balance that increases upon order submission

--- db/data/create.py
+++ db/data/create.py
@@ -259,3 +259,4 @@ createUser()
 # createBuyers()
 createSellers()
 createProducts()
+                                                                      
--- db/load.sql
+++ db/load.sql
@@ -1,7 +1,6 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Category FROM 'data/Category.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Product FROM 'data/Product.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY Buyer FROM 'data/Buyer.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Seller FROM 'data/Seller.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Selling FROM 'data/Selling.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Cart FROM 'data/Cart.csv' WITH DELIMITER ',' NULL '' CSV
